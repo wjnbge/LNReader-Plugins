a60c166e538ae44aa381be74bd36b265a9fd54ae
LNReader Plugin Dev <plugin@local.host>
2026-06-23 14:36:33 +0800

    Remove test and dev files

--- bqg998/test_generate.py
+++
@@ -1,9 +0,0 @@
-import re
-with open('bqg998.js', 'r', encoding='utf-8') as f:
-    code = f.read()
-
-code = re.sub(r'require\([\'\"].*?[\'\"]\)', '{}', code)
-code += '\nconsole.log(exports.default.getToken(\'62714\', 1));\n'
-
-with open('test.js', 'w', encoding='utf-8') as f:
-    f.write(code)
--- test_alicesw3.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import re
-
-req=urllib.request.Request('https://www.alicesw.com/all/order/update_time+desc.html', headers={'User-Agent':'Mozilla/5.0'})
-html=urllib.request.urlopen(req).read().decode('utf-8')
-start = html.find('<ul class="itemg">')
-end = html.find('</ul>', start)
-print(html[start:start+1000])
